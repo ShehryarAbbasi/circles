9ff7ee064e277f55aa5596d472b99008e621b56d
ShehryarAbbasi <stockdown25@gmail.com>
2014-12-01 05:03:30 -0600

    extended capability

--- custom.js
+++ custom.js
@@ -1,6 +1,6 @@
 (function () {   																		// wrap in IIFE
-	var url = "https://api.myjson.com/bins/3zpvj"  										// created JSON, with 3 circles in an array, "name", "color", "diameter"
-	var urlOriginal = "http://opslocal-c01.dotomi.com/update/local/swfs/circles.json";  // not working .. receiving DNS error possibly, check Server settings/resources
+	var url = "https://api.myjson.com/bins/3h2kv"  										// created JSON, with circles in an array, "name", "color", "diameter"
+	var urlOriginal = "http://opslocal-c01.dotomi.com/update/local/swfs/circles.json";  // not working (tried callback) receiving Server DNS error possibly
 	var myData = $.getJSON(url, function(data) {										// get the data
 	            console.log(data);														// log received data for identifying key:value pairs
 	    $.each(data.circles, function(i, item) {										// iterate through the items in the array
@@ -10,36 +10,44 @@
 	            "border-radius": item.diameter/2+"px",									// divide by 2 to make this shape resemble a circle
 	            "background-color": item.color 											// set background color of div
 	        };
-	        $("#"+item.name).css(circleProperties);										// set CSS styles for each item.name
+	        var addDiv = '<div class="circle" id="' +item.name+ '"></div>';				// dynamically build div tags based on JSON items
+            $("#container").append(addDiv);												// add children divs to the parent div
+            $("#"+item.name).css(circleProperties);										// set CSS styles for each div id
 	    });
-	    setInterval(move,5000);															// call this function every 5 seconds for continuous animation
+	    binder();																		// call the click event handler
+     	setInterval(move, 5000);														// call this function every 5 seconds for continuous animation
 	});
 
-	$(".circle").click(function(){																	// watch this div class for clicks
-	    var id = $(this).attr("id");																// get the div id of what was clicked
-	    var newBgColor = ["yellow","pink","gray", "lavender", "lawngreen", "magenta", "maroon"];	// fill an array with some "random" colors
-	    var randomColor = newBgColor[Math.floor(Math.random()*newBgColor.length)];					// pick an index randomly from color array
-	    $("#"+id).css("background-color", randomColor);												// change the color of the clicked div id
-	});
+	function binder() {
+		$(".circle").click(function(){																	// watch this div class for clicks
+		    var id = this.id;																			// get the div id of what was clicked
+		    var newBgColor = ["yellow","pink","gray","lavender","lawngreen","magenta","maroon"];		// fill an array with some "random" colors
+		    var randomColor = newBgColor[Math.floor(Math.random()*newBgColor.length)];					// pick an index randomly from color array
+		    $("#"+id).css("background-color", randomColor);												// change the color of the clicked div id
+		});
+	}
 
 	function Animation(circleName,circleSpeed,circleDistance) {							// constructor function for (queued) jquery animation
 	 	this.circleName = circleName;													
 	 	this.circleSpeed = circleSpeed;
 	 	this.circleDistance = circleDistance;
-	 	$("#"+circleName).animate({left: "+="+circleDistance+"px"}, circleSpeed).animate({left: "-="+circleDistance+"px"}, circleSpeed);
+	 	$("#"+circleName).animate({top: "+="+circleDistance+"px"}, circleSpeed).animate({top: "-="+circleDistance+"px"}, circleSpeed);
 	 }  
 
 	function move() {  																	// this function instantiates a circle's animation
-	    var speed1 = 2500, speed2 = 3500, speed3 = 4500;								// variables to store various speeds
-	    var dist = 400;																	// variable to store the css distance "px" the circle will travel
-	    var cir1 = new Animation("circle1",speed1,dist);								// instanstiates a circle's animation; passes div id, speed, & distance 
-	    var cir2 = new Animation("circle2",speed2,-dist);								// negative distance, to move this circle in reverse direction than others
-	    var cir3 = new Animation("circle3",speed3,dist);								//
-	 }
+	    var speed = [2500,3500,4500];													// this array holds different speeds for a circle
+	    var dist = [200,400,600];														// this array holds different distances for a circle
+        $.each($(".circle"), function () {												// iterate over each div id inside the .circle class
+	        var randomSpeed = speed[Math.floor(Math.random()*speed.length)];			// each iteration randomly picks a value from the speed array
+	        var randomDist = dist[Math.floor(Math.random()*dist.length)];				// each iteration randomly picks a value from the distance array
+	        var cirId = this.id;														// each iteration sets the current div id which it is looking at
+		    var cirAnime = new Animation(cirId,randomSpeed,randomDist);					// each iteration instantiates an animation sequence 
+	 	});
+	}
 }()); 																					
 
 
-/*---- this is a copy of the JSON data which was uploaded to myjson.com------
+/*---- this is a copy of the JSON data format which was uploaded to myjson.com------
 {
     "circles": [
         {
